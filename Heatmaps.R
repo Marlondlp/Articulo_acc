caf60933be2962d2b46b721a5c77c8c3d997ed6d
Marlondlp <malondlp@yahoo.es>
2021-06-03 14:47:54 -0500

    v2

--- Heatmaps.R
+++ Heatmaps.R
@@ -1,1 +1,24 @@
-#
+##subir archivo de datos
+DatosBurdeosDMdef12_05_2021 <- read_excel("C:/Users/malon/Dropbox/Tesis PhD/Tesis escrita/Datos Burdeos/DatosBurdeosDMdef12.05.2021.xlsx", 
+                                          +     sheet = "DatosBurdeosDMdef")
+
+
+#Cargar librerias
+library(tidyr)
+
+#Crear un DataFrame con el promedio de los datos teniedo en cuenta el organo y fuente de nitrogeno
+DatosBurdeosDMdef12_05_2021_unite<-unite(DatosBurdeosDMdef12_05_2021,Treatment, c("Source", "Accesion", 
+                                                                                  "Organ"),sep="_", remove=FALSE)
+DatosBurdeosDMdef12_05_2021_Mean<-aggregate(DatosBurdeosDMdef12_05_2021_unite[,5:33], by=list(DatosBurdeosDMdef12_05_2021_unite$Treatment), mean, 
+                                            na.rm=TRUE)
+
+#separar las variables de la columna Group.1
+sepmean.Datos<-separate(DatosBurdeosDMdef12_05_2021_Mean,Group.1,into=c("Source", "Accesion", 
+                                                                        "Organ" ),sep="_")
+#Crear Dataframes con los datos de hojas y raiz por separado
+hojas_mean<-subset(sepmean.Datos, Organ=="leaves")
+raiz_mean<-subset(sepmean.Datos, Organ=="Root")
+
+#Separar los datos de amonio
+A_hojas_mean<-subset(hojas_mean, Source=="A")
+A_raiz_mean<-subset(raiz_mean, Source=="A")
