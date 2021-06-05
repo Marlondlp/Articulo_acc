be21606eeb46db436e33bbd7ed4b4ce4ae4d785f
Marlondlp <malondlp@yahoo.es>
2021-06-05 10:44:53 -0500

    v6

--- Heatmaps.R
+++ Heatmaps.R
@@ -30,21 +30,39 @@ N_raiz_mean<-subset(raiz_mean, Source=="N")
 
 #Nombre de los rows
 rownames(A_hojas_mean)=A_hojas_mean$Accesion
+rownames(A_raiz_mean)=A_raiz_mean$Accesion
+rownames(N_hojas_mean)=N_hojas_mean$Accesion
+rownames(N_raiz_mean)=N_raiz_mean$Accesion
 
 
 
 #convertir a matrix
 matrix_A_hojas_mean<-as.matrix(A_hojas_mean[,c(-1:-7,-23,-31,-32)])
+matrix_A_raiz_mean<-as.matrix(A_raiz_mean[,c(-1:-8,-18,-23,-31,-32)])
+matrix_N_hojas_mean<-as.matrix(N_hojas_mean[,c(-1:-7,-23,-31,-32)])
+matrix_N_raiz_mean<-as.matrix(N_raiz_mean[,c(-1:-8,-18,-23,-31,-32)])
+
 #Trabajar con los datos estandarizados por accesiones
 scaled_matrix_A_hojas_mean<-scale(sqrt(matrix_A_hojas_mean), center = TRUE, scale = TRUE)
 
 Heatmap(scaled_matrix_A_hojas_mean, row_names_gp = gpar(fontsize = 4), scale = "none")
 
 
-rowCols <- ifelse(rownames(scaled_matrix_A_hojas_mean)=="Uni2","purple","red")
-
+rowCols <- ifelse(rownames(scaled_matrix_A_hojas_mean)=="Koz-3"|rownames(scaled_matrix_A_hojas_mean)=="BdTR12c"|rownames(scaled_matrix_A_hojas_mean)=="Arn1"|rownames(scaled_matrix_A_hojas_mean)=="BdTR11G"|rownames(scaled_matrix_A_hojas_mean)=="S8iiC"|rownames(scaled_matrix_A_hojas_mean)=="BdTR8i"|rownames(scaled_matrix_A_hojas_mean)=="ABR7"|rownames(scaled_matrix_A_hojas_mean)=="BdTR5I"|rownames(scaled_matrix_A_hojas_mean)=="Mon3"|rownames(scaled_matrix_A_hojas_mean)=="BdTR3C"|rownames(scaled_matrix_A_hojas_mean)=="BdTR13C"|rownames(scaled_matrix_A_hojas_mean)=="BdTR11I"|rownames(scaled_matrix_A_hojas_mean)=="Gaz-8"|rownames(scaled_matrix_A_hojas_mean)=="BdTR2B"|rownames(scaled_matrix_A_hojas_mean)=="BdTR10C"|rownames(scaled_matrix_A_hojas_mean)=="Mur1"|rownames(scaled_matrix_A_hojas_mean)=="Per1"|rownames(scaled_matrix_A_hojas_mean)=="Mig3"|rownames(scaled_matrix_A_hojas_mean)=="Koz-1"|rownames(scaled_matrix_A_hojas_mean)=="BdTR1i","purple","red")
+rowCols
 
 heatmap.2(scaled_matrix_A_hojas_mean,  scale = "none", col="bluered",hclustfun = function(x) hclust(x, method="ward.D2"),cexCol = 0.6,cexRow = 0.5,  RowSideColors=rowCols)
+
+
+
+#trabajar con el fold change(A/N)
+FC_hoja<-log2(matrix_A_hojas_mean)-log2(matrix_N_hojas_mean)
+rowCols <- ifelse(rownames(FC_hoja)=="Koz-3"|rownames(FC_hoja)=="BdTR12c"|rownames(FC_hoja)=="Arn1"|rownames(FC_hoja)=="BdTR11G"|rownames(FC_hoja)=="S8iiC"|rownames(FC_hoja)=="BdTR8i"|rownames(FC_hoja)=="ABR7"|rownames(FC_hoja)=="BdTR5I"|rownames(FC_hoja)=="Mon3"|rownames(FC_hoja)=="BdTR3C"|rownames(FC_hoja)=="BdTR13C"|rownames(FC_hoja)=="BdTR11I"|rownames(FC_hoja)=="Gaz-8"|rownames(FC_hoja)=="BdTR2B"|rownames(FC_hoja)=="BdTR10C"|rownames(FC_hoja)=="Mur1"|rownames(FC_hoja)=="Per1"|rownames(FC_hoja)=="Mig3"|rownames(FC_hoja)=="Koz-1"|rownames(FC_hoja)=="BdTR1i","purple","red")
+heatmap.2(FC_hoja,  scale = "none", col="bluered",hclustfun = function(x) hclust(x, method="ward.D2"),cexCol = 0.6,cexRow = 0.5,  RowSideColors=rowCols)
+
+scaled_FC_hoja<-scale(FC_hoja, center = TRUE, scale = TRUE)
+heatmap.2(scaled_FC_hoja,  scale = "none", col="bluered",hclustfun = function(x) hclust(x, method="ward.D2"),cexCol = 0.6,cexRow = 0.5,  RowSideColors=rowCols)
+
 
 
 library(circlize)
